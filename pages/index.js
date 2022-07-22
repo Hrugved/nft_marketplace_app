1001a598856cf9c764f12d085fbba1526d893fb6
hrugved <whrugved@gmail.com>
2022-07-22 14:38:15 +0530

    replace moralis server with thegraph

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,27 @@ import Image from "next/image"
 import { useMoralis, useMoralisQuery } from "react-moralis"
 import NFTBox from "../components/NFTBox"
 import styles from "../styles/Home.module.css"
+import networkMapping from "../constants/networkMapping.json"
+import { useQuery } from "@apollo/client"
+import GET_ACTIVE_ITEMS from "../constants/subgraphQueries"
 
 export default function Home() {
+  const { chainId, isWeb3Enabled } = useMoralis()
+  const chainString = chainId ? parseInt(chainId) : "31337"
+  const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
 
-  const {isWeb3Enabled} = useMoralis()
-
-  const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
-    "ActiveItem",
-    (query) => query.limit(10).descending("tokenId")
-  )
+  const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS)
 
   return (
     <div className="container mx-auto">
       <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
       <div className="flex flex-wrap gap-8">
         {isWeb3Enabled ? (
-          fetchingListedNfts ? (
+          loading || !listedNfts ? (
             <div>Loading...</div>
           ) : (
-            listedNfts.map((nft) => {
-              const { price, nftAddress, tokenId, marketplaceAddress, seller } = nft.attributes
+            listedNfts.activeItems.map((nft) => {
+              const { price, nftAddress, tokenId, seller } = nft
               return (
                 <NFTBox
                   price={price}
